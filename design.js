ed3de16413301632f4ad688adc011f478d64c615
Sayeeshwar <sayeeshwar.girish@gmail.com>
2018-03-24 16:29:18 +0530

    Increment set variable on clicking #submit and get row and column values

--- design.js
+++ design.js
@@ -4,5 +4,8 @@ var column = $('#column').val();
 var curCol=column;
 var set=0;
 $('#submit').click(function makeGrid(){
-    
+    set++;
+    row = $('#row').val();
+    column = $('#column').val();
+
 });
